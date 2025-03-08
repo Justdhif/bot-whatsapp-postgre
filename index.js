f90a2845d069ed53e79f17ec8fe8989421180b1e
Justdhif <noob1234five@gmail.com>
2025-03-08 23:08:47 +0700

    fix: Index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
